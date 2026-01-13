0275fb4809c93ab87a37c3d1b087b69653b7bf5b
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-13 12:17:32 +0800

    Problem1

--- array1.java
+++ array1.java
@@ -1,18 +1,17 @@
-class main array1 {
+import java.util.Scanner;
+    class array1 {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i = 0; i <= arr.length - 1; i++) {
             System.out.println(arr[i]);
         }
-        Scanner sc = new Scanner(System.in);
-
 
         System.out.print("Enter an number in the array: ");
         int chosen = sc.nextInt;
-        
 
         for (int equal = 0; equal <= arr.length - 1; equal++){
-            if (arr[equal] =  chosen) {
+            if (arr[equal] ==  chosen) {
                 System.out.println(equal);
                 break;
             }
