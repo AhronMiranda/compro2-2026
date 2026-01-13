92dff6252c59ad37a63850ea8291b4d9e84f7944
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-13 11:46:36 +0800

    lmaoo

--- array1.java
+++ array1.java
@@ -1,7 +1,7 @@
 class main array1 {
     public static void main(String args[]) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int i = 0; i <= arr.length; i++) {
+        for (int i = 0; i <= arr.length - 1; i++) {
             System.out.println(arr[i]);
         }
     }
