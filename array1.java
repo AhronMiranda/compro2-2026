3c5a56e7a2ab6d405de76a130b73c957ccc27228
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-13 12:28:29 +0800

    I finished the array search code

--- array1.java
+++ array1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
         }
 
         System.out.print("Enter an number in the array: ");
-        int chosen = sc.nextInt;
+        int chosen = sc.nextInt();
 
         for (int equal = 0; equal <= arr.length - 1; equal++){
             if (arr[equal] ==  chosen) {
