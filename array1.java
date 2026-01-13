0b1a3c3875dbad64e66fa0b151b2c028605c2e1f
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-13 11:42:26 +0800

    Array

---
+++ array1.java
@@ -0,0 +1,8 @@
+class main array1 {
+    public static void main(String args[]) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i <= arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
