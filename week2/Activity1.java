b66b1e3af4297c2a53d8443017f997f486cdf672
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-19 12:14:17 +0800

    COMPRO Week 1

---
+++ week2/Activity1.java
@@ -0,0 +1,21 @@
+package week2;
+
+public class Activity1 {
+    public static void main(String[] args) {
+
+        int[] theaterRow = new int[8];
+        theaterRow[3] = 1;
+
+        int count = 0;
+        for (int i = 0; i < theaterRow.length - 1; i++) {
+            if (theaterRow[i] == 1) {
+                System.out.println("Seat " + i + " is Taken");
+                count += 1;
+            } else {
+                System.out.println("Seat " + i + " Available");
+            }
+            
+        }
+        System.out.println("Number of Available seats: " + (theaterRow.length - 1));
+    }
+}
