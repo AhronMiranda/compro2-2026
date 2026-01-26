a414f349ff6b9fc333ba7865e025388e4911e2c9
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-01-26 11:08:37 +0800

    fixed repo

--- practice/week1/array1.java
+++ practice/week1/array1.java
@@ -1,8 +1,10 @@
+
+
 import java.util.Scanner;
     class array1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 22, 333, 4444, 55555, 666666, 7777777, 88888888, 999999999, 1010};
         for (int i = 0; i <= arr.length - 1; i++) {
             System.out.print(arr[i] + ", ");
         }
