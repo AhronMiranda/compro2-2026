23cc8652b77d4b2c0e3c5ffef5fbc91d4d8c7f61
AhronMiranda <ahronjieannekent.miranda@lorma.edu>
2026-02-02 11:54:34 +0800

    compro

--- practice/week4/Main.java
+++ practice/week4/Main.java
@@ -1,5 +1,44 @@
+package week4;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
+        // try (FileWriter fw = new FileWriter("data.txt") ){ // SAVE FILE 
+        //     //fw = new FileWriter("data.txt"); //SAVE FILE
+        //     fw.write("Anything you want to save"); //CONTENT
+        //     fw.flush();  //UPLOAD
+        // } catch (IOException e) {
+        //     System.out.println(e.getMessage());
+        // } finally {
+        //     // fw.close();
+        // }
+        
+
+        StringBuilder sb = new StringBuilder();
+        
+        System.out.println("Enter something: ");
+        try (Scanner sc = new Scanner(System.in)) {
+            sb.append(sc.nextLine());
+        } catch (InputMismatchException e) {
+            System.out.println("invalid input");
+        }
+        
+        try (FileWriter fw = new FileWriter("dataa.txt")) {
+            fw.write(sb.toString());
+            System.out.println("Data is saved...");
+            fw.flush();
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
     }
 }
