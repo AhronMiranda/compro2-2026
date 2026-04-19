a3be7ba97c23bfe81cf7e637caa827c977358818
Ahron Miranda <ahronjieannekent.miranda@lorma.edu>
2026-04-19 10:51:12 +0800

    REVISED

--- activity7/App.java
+++ activity7/App.java
@@ -9,91 +9,100 @@ import activity7.services.PhonebookService;
 public class App {
     public static Scanner sc = new Scanner(System.in);
     public static String filename = "contacts.csv";
+
     public static void main(String[] args) {
         PhonebookService PS = new PhonebookService();
         Contact c = new Contact();
+
+        PS.loadFromCSV(filename);   
+
         menu(PS, c);
     }   //MAIN
 
     public static void menu(PhonebookService PS, Contact c) {
-            menu();
+        menu();
         while (true) {
             try {
-        System.out.print("user>>");
-        int answer = sc.nextInt(); sc.nextLine();
-        
-        switch (answer) {
-            case 0:
-                System.exit(0);
-                break;
-            case 1:
-                setContact(c);
-                System.out.println(c.toCsvString());
-                PS.addContact(c);
-                PS.saveToCSV(filename);
-                menu();
-                break;
-            case 2:
-                String strSearch = "";
-                try {
-                    System.out.print("Input Contact Number>>");
-                    strSearch = sc.nextLine();
-                    PS.searchContact(strSearch);
-                    menu();
-                } catch (InputMismatchException e) {
-                System.out.println("[ERROR] INVALID INPUT");
-            }
-                //PS.searchContact(strSearch);
-                break;
-            case 3:
-                String strRemove = "";
-                try {
-                    System.out.print("Input Contact Number>");
-                    strRemove = sc.nextLine();
-                    PS.removeContact(strRemove);
-                    
-                    System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
-                } catch (InputMismatchException e) {
-                System.out.println("[ERROR] INVALID INPUT");
-            }
+                System.out.print("user>>");
+                int answer = sc.nextInt(); 
+                sc.nextLine();
 
-                //PS.removeContact(strRemove);
-                break;
-            case 4:
-                PS.loadFromCSV(filename);
-                System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
-                break;
-            case 5:
-                PS.saveToCSV(filename);
-                System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
-                break;
+                switch (answer) {
+                    case 0:
+                        System.exit(0);
+                        break;
+
+                    case 1:
+                        c = new Contact();   
+                        setContact(c);
+                        System.out.println(c.toCsvString());
+                        PS.addContact(c);
+                        PS.saveToCSV(filename);
+                        menu();
+                        break;
+
+                    case 2:
+                        String strSearch = "";
+                        try {
+                            System.out.print("Input Contact Name>>");  
+                            strSearch = sc.nextLine();
+                            PS.searchContact(strSearch);
+                            menu();
+                        } catch (InputMismatchException e) {
+                            System.out.println("[ERROR] INVALID INPUT");
+                        }
+                        break;
+
+                    case 3:
+                        String strRemove = "";
+                        try {
+                            System.out.print("Input Contact Name>>");   
+                            strRemove = sc.nextLine();
+                            PS.removeContact(strRemove);
+
+                            System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
+                        } catch (InputMismatchException e) {
+                            System.out.println("[ERROR] INVALID INPUT");
+                        }
+                        break;
+
+                    case 4:
+                        PS.printFromCSV(filename);
+                        System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
+                        break;
+
+                    case 5:
+                        PS.saveToCSV(filename);
+                        System.out.println("1. Add | 2. Search | 3. Remove | 4. Display All | 5. Save to CSV | 0. Exit");
+                        break;
+                }
+
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("[ERROR] INVALID");
+            }
         }
-    } catch (InputMismatchException e) {
-        sc.nextLine();
-        System.out.println("[ERROR] INVALID");
-    }
-    }
     }   // MENU
 
     public static void setContact(Contact c) {
         while (true) {
             try {
-        System.out.print("Input Contact Name>>");
-        c.setName(sc.nextLine());
+                System.out.print("Input Contact Name>>");
+                c.setName(sc.nextLine());
             } catch (InputMismatchException e) {
                 System.out.println("[ERROR] INVALID INPUT");
             }
 
             try {
-        System.out.print("Input Contact Phone Number>>");
-        c.setPhoneNumber(sc.nextLine());
+                System.out.print("Input Contact Phone Number>>");
+                c.setPhoneNumber(sc.nextLine());
             } catch (InputMismatchException e) {
                 System.out.println("[ERROR] INVALID INPUT");
             }
 
             try {
-        System.out.print("Input Contact Email>>");
-        c.setEmail(sc.nextLine());
+                System.out.print("Input Contact Email>>");
+                c.setEmail(sc.nextLine());
             } catch (InputMismatchException e) {
                 System.out.println("[ERROR] INVALID INPUT");
             }
--- activity7/services/PhonebookService.java
+++ activity7/services/PhonebookService.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.io.File;
 
 import activity7.models.Contact;
 
@@ -13,31 +14,26 @@ public class PhonebookService {
     private HashMap<String, Contact> contacts = new HashMap<>();
 
     public void addContact(Contact c) {
-        contacts.put(c.getPhoneNumber(), c) ;
+        contacts.put(c.getName(), c);   
         System.out.println("[CONTACT ADDED]");
-        
-
     }
 
     public void searchContact(String name){
-        Contact c = contacts.get(name);
-    if (c != null) {
-        System.out.println("[CONTACT FOUND]");
-        System.out.println(c);
-    } else {
-        System.out.println("[CONTACT NOT FOUND]");
-    }
-
+        Contact c = contacts.get(name);   
+        if (c != null) {
+            System.out.println("[CONTACT FOUND]");
+            System.out.println(c);
+        } else {
+            System.out.println("[CONTACT NOT FOUND]");
+        }
     }
 
     public void removeContact(String name) {
-        contacts.remove(name);
+        contacts.remove(name);   
         System.out.println("[CONTACT REMOVED]");
-
     }
 
     public void saveToCSV(String filename) {
-        
         try (BufferedWriter BF = new BufferedWriter(new FileWriter(filename))) {
             for (Contact c : contacts.values()) {
                 BF.write(c.toCsvString());
@@ -47,30 +43,66 @@ public class PhonebookService {
         } catch (IOException e) {
             System.out.println("[ERROR] IOException");
         }
-        
     }
 
-    public void loadFromCSV(String filename) {
-        contacts.clear();
-        String line;
-        try (BufferedReader BR = new BufferedReader(new FileReader(filename))) {
-            while ((line = BR.readLine()) != null) {
+   public void loadFromCSV(String filename) {
+    contacts.clear();
+    String line;
+
+    File file = new File(filename);
+
+    if (!file.exists()) {
+        System.out.println("[NO FILE FOUND - STARTING NEW PHONEBOOK]");
+        return;
+    }
+
+    try (BufferedReader BR = new BufferedReader(new FileReader(file))) {
+        while ((line = BR.readLine()) != null) {
             String[] values = line.split(",");
             if (values.length == 3) {
                 Contact c = new Contact();
                 c.setName(values[0]);
                 c.setPhoneNumber(values[1]);
                 c.setEmail(values[2]);
-                contacts.put(c.getName(), c);
 
+                contacts.put(c.getName(), c);
+            }
+        }
+        System.out.println("[LOADED]");
+
+    } catch (IOException e) {
+        System.out.println("[ERROR] IOException reading the CSV file");
+    }
+}
+
+   public void printFromCSV(String filename) {
+    contacts.clear();
+    String line;
+
+    File file = new File(filename);
+
+    if (!file.exists()) {
+        System.out.println("[NO FILE FOUND - STARTING NEW PHONEBOOK]");
+        return;
+    }
+
+    try (BufferedReader BR = new BufferedReader(new FileReader(file))) {
+        while ((line = BR.readLine()) != null) {
+            String[] values = line.split(",");
+            if (values.length == 3) {
+                Contact c = new Contact();
+                c.setName(values[0]);
+                c.setPhoneNumber(values[1]);
+                c.setEmail(values[2]);
+
+                contacts.put(c.getName(), c);
                 System.out.println(c);
             }
         }
         System.out.println("[LOADED]");
 
-        } catch (IOException e) {
-            System.out.println("[ERROR] IOException reading the CSV file");
-        }
-        
+    } catch (IOException e) {
+        System.out.println("[ERROR] IOException reading the CSV file");
     }
+}
 }
